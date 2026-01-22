6f0de7745e6f17702ab145243d925f8c4e26430a
cesar80808 <cesar.flores@cimat.mx>
2026-01-22 00:32:05 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import my_io 
 from artgallprob import solve_art_gallery_problem, display_solution
 
-input_filename = "input/points2.txt"
-output_filename = "output/solution2.txt"
+input_filename = "input/points1.txt"
+output_filename = "output/solution1.txt"
 
 def main():
     polygon = my_io.read_polygon(input_filename)
